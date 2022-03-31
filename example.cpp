b6bb62e5acca9ba61cea53c40268d9258b70dc24
chanceyx <seanchan0901@foxmail.com>
2022-03-31 18:26:41 +0800

    feat: add mutex limiter

--- example.cpp
+++ example.cpp
@@ -3,12 +3,44 @@
 #include "limiter_atomic.hpp"
 
 int main() {
-  AtomicLimiter limiter(100, std::chrono::seconds(1), 100);
+  std::cout << "<--------- atomic limiter --------->" << std::endl;
+  LimiterAtomic limiter_atomic(100, std::chrono::seconds(1), 100);
+  std::cout << "<---------- test limiter ---------->" << std::endl;
   auto prev = std::chrono::system_clock::now();
   for (int i = 0; i < 10; i++) {
-    auto now = limiter.take();
+    auto now = limiter_atomic.take();
     if (i > 0) std::cout << i << " " << std::chrono::duration_cast<std::chrono::milliseconds>(now - prev).count() << "ms" << std::endl;
     prev = now;
   }
+
+  std::cout << "<----------- test slack ----------->" << std::endl;
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  prev = std::chrono::system_clock::now();
+  for (int i = 0; i < 10; i++) {
+    auto now = limiter_atomic.take();
+    if (i > 0) std::cout << i << " " << std::chrono::duration_cast<std::chrono::milliseconds>(now - prev).count() << "ms" << std::endl;
+    prev = now;
+  }
+
+  std::cout << "\n";
+  std::cout << "<--------- mutex_ limiter --------->" << std::endl;
+  LimiterMutex limiter_mutex(100, std::chrono::seconds(1), 100);
+  std::cout << "<---------- test limiter ---------->" << std::endl;
+  prev = std::chrono::system_clock::now();
+  for (int i = 0; i < 10; i++) {
+    auto now = limiter_mutex.take();
+    if (i > 0) std::cout << i << " " << std::chrono::duration_cast<std::chrono::milliseconds>(now - prev).count() << "ms" << std::endl;
+    prev = now;
+  }
+
+  std::cout << "<----------- test slack ----------->" << std::endl;
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+  prev = std::chrono::system_clock::now();
+  for (int i = 0; i < 10; i++) {
+    auto now = limiter_mutex.take();
+    if (i > 0) std::cout << i << " " << std::chrono::duration_cast<std::chrono::milliseconds>(now - prev).count() << "ms" << std::endl;
+    prev = now;
+  }
+
   return 0;
 }
